6f6ad5f92978986f529942508b575fff9e963eda
Ashwath R <ashwath.ramanan@mygate.in>
2018-08-07 07:32:00 +0530

    Initial Commit

---
+++ FlickerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlickerApp
+//
+//  Created by Ashwath R on 07/08/18.
+//  Copyright © 2018 Ashwath R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
